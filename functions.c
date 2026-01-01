03d2e42b64266e5d6d3bbee74240a855b73d4410
Micael Velez <mvrod2314@gmail.com>
2026-01-01 19:51:51 +0000

    functions

--- functions.c
+++ functions.c
@@ -1,18 +1,15 @@
 #include "shell.h"
 
 /**
- * env_fetch - Verifies tokens.
- * @args: String of arguments given.
- * @input: User input string.
- * @count: Command counter.
+ * env_fetch - Handle builtins and execute commands
+ * @args: Array of command arguments
+ * @input: User input string
+ * @count: Command counter
  *
- * Return: -1 on exit, 0 otherwise
+ * Return: -1 if "exit" command, 0 otherwise
  */
 int env_fetch(char **args, char *input, int count)
 {
-	char **env = environ;
-	int i = 0;
-
 	(void)input;
 
 	if (strcmp(args[0], "exit") == 0)
@@ -20,84 +17,77 @@ int env_fetch(char **args, char *input, int count)
 
 	if (strcmp(args[0], "env") == 0)
 	{
-		while (env[i])
-		{
-			printf("%s\n", env[i]);
-			i++;
-		}
+		int i = 0;
+
+		while (environ[i])
+			printf("%s\n", environ[i++]);
 		return (0);
 	}
 
 	if (find_or_execute_command(args) == -1)
-	{
 		printf("./hsh: %d: %s: not found\n", count, args[0]);
-	}
 
 	return (0);
 }
 
 /**
- * _getenv - Finds the value of an environment variable.
- * @name: Name of the variable.
+ * _getenv - Get value of environment variable
+ * @name: Name of the environment variable
  *
  * Return: Pointer to value string, or NULL if not found
  */
 char *_getenv(const char *name)
 {
-	size_t length = strlen(name);
+	size_t len = strlen(name);
 	char **env = environ;
 
-	while (*env != NULL)
+	while (*env)
 	{
-		if (strncmp(*env, name, length) == 0 && (*env)[length] == '=')
-			return (&(*env)[length + 1]);
+		if (strncmp(*env, name, len) == 0 && (*env)[len] == '=')
+			return (*env + len + 1);
 		env++;
 	}
+
 	return (NULL);
 }
 
 /**
- * parse_input - Tokenizes user input into arguments.
- * @input: User input string.
+ * parse_input - Tokenize user input into arguments
+ * @input: User input string
  *
- * Return: Array of strings (tokens)
+ * Return: Array of string tokens
  */
 char **parse_input(char *input)
 {
-	char **arguments = malloc(MAX_ARGS * sizeof(char *));
+	char **args = malloc(MAX_ARGS * sizeof(char *));
 	char *token;
-	int number_of_arguments = 0;
+	int i = 0;
 
-	if (!arguments)
-	{
-		perror("malloc");
+	if (!args)
 		exit(EXIT_FAILURE);
-	}
 
 	token = strtok(input, " \n");
-	while (token != NULL && number_of_arguments < MAX_ARGS - 1)
+	while (token && i < MAX_ARGS - 1)
 	{
-		arguments[number_of_arguments] = token;
+		args[i++] = token;
 		token = strtok(NULL, " \n");
-		number_of_arguments++;
 	}
-	arguments[number_of_arguments] = NULL;
+	args[i] = NULL;
 
-	return (arguments);
+	return (args);
 }
 
 /**
- * execute_command - Forks and executes a command.
- * @args: Array of arguments
+ * execute_command - Fork and execute a command
+ * @args: Array of command arguments
  *
- * Return: -1 on fork failure, 0 otherwise
+ * Return: -1 if fork fails, 0 otherwise
  */
 int execute_command(char **args)
 {
-	pid_t pid;
+	pid_t pid = fork();
 	int status;
 
-	pid = fork();
 	if (pid == -1)
 		return (-1);
 
@@ -106,46 +96,52 @@ int execute_command(char **args)
 		execve(args[0], args, environ);
 		exit(EXIT_FAILURE);
 	}
-	else
-	{
-		wait(&status);
-	}
+
+	wait(&status);
+
 	return (0);
 }
 
-/* Prototype for helper in helpers.c */
-int execute_in_path(char *dir, char **args, char *path_copy);
-
 /**
- * find_or_execute_command - Checks PATH and executes command if found.
- * @args: Command and arguments array
+ * find_or_execute_command - Execute command using PATH if needed
+ * @args: Array of command arguments
  *
- * Return: 0 if executed, -1 if not found
+ * Return: 0 if executed successfully, -1 if command not found
  */
 int find_or_execute_command(char **args)
 {
-	char *path, *path_copy, *dir;
+	char *path = _getenv("PATH");
+	char *copy, *dir, *cmd;
+	size_t len;
 
 	if (access(args[0], X_OK) == 0)
 		return (execute_command(args));
 
-	path = _getenv("PATH");
 	if (!path)
 		return (-1);
 
-	path_copy = strdup(path);
-	if (!path_copy)
+	copy = strdup(path);
+	if (!copy)
 		return (-1);
 
-	dir = strtok(path_copy, ":");
-	while (dir)
+	for (dir = strtok(copy, ":"); dir; dir = strtok(NULL, ":"))
 	{
-		if (execute_in_path(dir, args, path_copy) == 0)
-			return (0);
+		cmd = malloc(strlen(dir) + strlen(args[0]) + 2);
+		if (!cmd)
+			continue;
 
-		dir = strtok(NULL, ":");
+		sprintf(cmd, "%s/%s", dir, args[0]);
+		if (access(cmd, X_OK) == 0)
+		{
+			args[0] = cmd;
+			execute_command(args);
+			free(cmd);
+			free(copy);
+			return (0);
+		}
+		free(cmd);
 	}
 
-	free(path_copy);
+	free(copy);
 	return (-1);
 }
