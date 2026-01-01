da261e3782840b6025109b94190f2f3187ff5894
Cristian Acevedo <acecristian35@gmail.com>
2026-01-01 18:01:06 +0100

    Writes a UNIX command line interpreter

--- shell.c
+++ shell.c
@@ -4,79 +4,73 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h>
 
 #define MAX_INPUT_SIZE 1024
 #define PROMPT "#cisfun$ "
 
-// Function to execute the command
+/**
+ * execute_command - executes a command using execve
+ * @command: command to execute
+ *
+ * Return: void
+ */
 void execute_command(char *command)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    // Fork the process to create a child
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("Fork failed");
-        return;
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Fork failed");
+		return;
+	}
 
-    if (pid == 0)
-    { // Child process
-        // Use execve to run the command
-        char *argv[] = {command, NULL};
-        if (execve(command, argv, environ) == -1)
-        {
-            perror(command); // Print error if execve fails
-        }
-        exit(1); // Exit child process
-    }
-    else
-    { // Parent process
-        // Wait for the child to finish execution
-        waitpid(pid, &status, 0);
-    }
+	if (pid == 0)
+	{
+		char *argv[] = {command, NULL};
+
+		if (execve(command, argv, environ) == -1)
+			perror(command);
+
+		exit(1);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
 }
 
-int main()
+/**
+ * main - entry point for simple shell
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-    char input[MAX_INPUT_SIZE];
+	char input[MAX_INPUT_SIZE];
 
-    while (1)
-    {
-        // Display the prompt
-        printf("%s", PROMPT);
+	while (1)
+	{
+		printf("%s", PROMPT);
 
-        // Read the user input
-        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL)
-        {
-            if (feof(stdin))
-            {
-                // Handle end of file (Ctrl+D)
-                printf("\n");
-                break;
-            }
-            perror("Input error");
-            continue;
-        }
+		if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL)
+		{
+			if (feof(stdin))
+			{
+				printf("\n");
+				break;
+			}
+			perror("Input error");
+			continue;
+		}
 
-        // Remove newline character from the input
-        input[strcspn(input, "\n")] = 0;
+		/* Remove newline character */
+		input[strcspn(input, "\n")] = 0;
 
-        // Check for exit condition
-        if (strcmp(input, "exit") == 0)
-        {
-            break;
-        }
+		if (strlen(input) > 0)
+			execute_command(input);
+	}
 
-        // If the input is not empty, execute the command
-        if (strlen(input) > 0)
-        {
-            execute_command(input);
-        }
-    }
-
-    return 0;
+	return (0);
 }
