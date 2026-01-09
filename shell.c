513cebe2f79a622f8f6e7e1bd6f2353911444b10
Micael Velez <mvrod2314@gmail.com>
2026-01-09 13:42:04 +0000

    shell

--- shell.c
+++ shell.c
@@ -39,13 +39,12 @@ int run_shell(int mode)
 
 		if (comparator == -1)
 		{
-			free(input);
-			exit(0);
+			break;
 		}
 	}
 
 	free(input);
-	return (0);
+	return (last_status);
 }
 
 /**
@@ -57,10 +56,10 @@ int main(void)
 {
 	int mode = isatty(STDIN_FILENO);
 
-	run_shell(mode);
+	int status = run_shell(mode);
 
 	if (mode)
 		printf("\n");
 
-	return (0);
+	return (status);
 }
